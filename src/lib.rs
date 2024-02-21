543de54cc89e9aeb9ad0edcbf1984014fe11dd01
malongshuai <mlongshuai@gmail.com>
2024-02-22 00:00:43 +0800

    change runtime dir to ba_app/{bian,bian_u}

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub const WS_BASE_URL: &str = "wss://stream.binance.com:9443";
 /// 程序目录($HOME)
 pub fn app_dir() -> Result<std::path::PathBuf, Box<dyn std::error::Error>> {
     let home = std::env::var("HOME")?;
-    let path = std::path::Path::new(&home).to_path_buf();
+    let path = std::path::Path::new(&home).join("ba_app").to_path_buf();
     std::fs::create_dir_all(&path)?;
     Ok(path)
 }
